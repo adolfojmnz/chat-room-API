e7d73d99510b6b1412756fb8a1f0259d4cfc0b5d
Adolfo Jimenez <adolfoj@protonmail.com>
2023-03-14 18:34:35 -0400

    Add MessageTest suit

--- msg/tests.py
+++ msg/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
 
-# Create your tests here.
+from accounts.models import CustomUser as User
+from msg.models import Message
+
+
+class MessageTest(TestCase):
+
+    def setUp(self):
+        user = User.objects.create(
+            username = 'test-user',
+            email = 'user@testsuit.com',
+            password = 'test$psswd',
+        )
+        user.save()
+        self.user = user
+        message = Message.objects.create(
+            sender = user,
+            body = 'test body message',
+        )
+        message.save()
+        self.message = message
+
+    def test_message(self):
+        self.assertTrue(Message.objects.filter(sender__username=self.user.username).exists())
+        self.assertEqual(Message.objects.get(sender__username=self.user.username).body, self.message.body)
