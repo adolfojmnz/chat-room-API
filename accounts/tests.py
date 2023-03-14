f38d296b4132f9ee093fad7ffc4459ed882df53a
Adolfo Jimenez <adolfoj@protonmail.com>
2023-03-14 18:24:52 -0400

    Add UserTest suit

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
-# Create your tests here.
+from accounts.models import CustomUser as User
+
+
+class TestUser(TestCase):
+
+    def setUp(self):
+        user = User.objects.create(
+            username = 'test-user',
+            email = 'user@testsuit.com',
+            password = 'test$psswd',
+        )
+        user.save()
+        self.user = user
+
+    def test_user(self):
+        self.assertEqual(User.objects.filter(username='test-user').exists(), True)
+        self.assertEqual(User.objects.get(username='test-user').email, self.user.email)
+        self.assertEqual(User.objects.get(username='test-user').password, self.user.password)
