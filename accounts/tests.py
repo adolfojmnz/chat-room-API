a6e257e0b3c51752a3ff670a691500de666c21ce
Adolfo Jimenez <adolfoj@protonmail.com>
2023-03-15 12:57:50 -0400

    Refactor: add CreateTestUserMixin class for further inheritance

--- accounts/tests.py
+++ accounts/tests.py
@@ -2,17 +2,22 @@ from django.test import TestCase
 
 from accounts.models import CustomUser as User
 
+class CreateTestUserMixin:
 
-class TestUser(TestCase):
-
-    def setUp(self):
+    def create_test_user(self):
         user = User.objects.create(
             username = 'test-user',
             email = 'user@testsuit.com',
             password = 'test$psswd',
         )
-        user.save()
-        self.user = user
+        return user
+
+
+class TestUser(CreateTestUserMixin, TestCase):
+
+    def setUp(self):
+        self.user = self.create_test_user()
+        self.user.save()
 
     def test_user(self):
         self.assertEqual(User.objects.filter(username='test-user').exists(), True)
