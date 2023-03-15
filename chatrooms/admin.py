caf9fa04e868c66beb1a9c85538a2221adfc1183
Adolfo Jimenez <adolfoj@protonmail.com>
2023-03-15 19:20:07 -0400

    Register ChatroomMessage and ChatMessage models

--- chatrooms/admin.py
+++ chatrooms/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from chatrooms.models import Chatroom, Chat, Topic
+from chatrooms.models import Chatroom, Chat, Topic, ChatroomMessage, ChatMessage
 
 admin.site.register(Chatroom)
 admin.site.register(Chat)
 admin.site.register(Topic)
+admin.site.register(ChatroomMessage)
+admin.site.register(ChatMessage)
