42ba2433cb4e323f762c2667c8679e07cf14cbac
Adolfo Jimenez <adolfoj@protonmail.com>
2023-03-15 19:33:18 -0400

    Create ChatroomMessageListView and ChatMessageListView

--- api/urls.py
+++ api/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('chatrooms', views.ChatListView.as_view(), name='chatrooms'),
     path('chats', views.ChatListView.as_view(), name='chats'),
     path('topics', views.TopicListView.as_view(), name='topics'),
+    path('chatroom-messages', views.ChatroomMessageListView.as_view(), name='chatroom-messages'),
+    path('chat-messages', views.ChatMessageListView.as_view(), name='chat-messages'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,13 +1,15 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from accounts.models import CustomUser as User
-from chatrooms.models import Chatroom, Chat, Topic
+from chatrooms.models import Chatroom, Chat, Topic, ChatroomMessage, ChatMessage
 
 from api.serializers import (
     UserSerializer,
     ChatroomSerializer,
     ChatSerializer,
     TopicSerializer,
+    ChatroomMessageSerializer,
+    ChatMessageSerializer,
 )
 
 
@@ -33,3 +35,15 @@ class TopicListView(ListCreateAPIView):
     model = Topic
     queryset = model.objects.all()
     serializer_class = TopicSerializer
+
+
+class ChatroomMessageListView(ListCreateAPIView):
+    model = ChatroomMessage
+    queryset = model.objects.all()
+    serializer_class = ChatroomMessageSerializer
+
+
+class ChatMessageListView(ListCreateAPIView):
+    model = ChatMessage
+    queryset = model.objects.all()
+    serializer_class = ChatMessageSerializer
