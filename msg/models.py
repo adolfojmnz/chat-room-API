3c7b120d55ec9df88171a2fca8d6e6468fd00b32
Adolfo Jimenez <adolfoj@protonmail.com>
2023-03-15 12:22:50 -0400

    Add Files model

--- msg/models.py
+++ msg/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
+from accounts.models import CustomUser as User
+
 
 class Message(models.Model):
-    sender = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, on_delete=models.PROTECT)
     body = models.CharField(max_length=1000)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.body[:160]
+
+
+class Files(models.Model):
+    file = models.FileField()
+    type_name = models.CharField(max_length=64)
+    max_size_kbytes = models.IntegerField(default=5120)
+    sender = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    def __str__(self) -> str:
+        return self.type_name
