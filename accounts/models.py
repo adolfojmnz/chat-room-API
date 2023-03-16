8c2187e7d3259a6566694736b3fd0f067ac7edb7
Adolfo Jimenez <adolfoj@protonmail.com>
2023-03-16 11:22:40 -0400

    Alter birthdate field: set auto_now=True

--- accounts/models.py
+++ accounts/models.py
@@ -6,23 +6,13 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     '''
     default fields:
-        username
-        first_name
-        last_name
-        email
-        password
-        groups
-        user_permissions
-        is_staff
-        is_active
-        is_superuser
-        last_login
-        date_joined
+        username, first_name, last_name, email, password, group, user_permissions,
+        is_staff, is_active, is_superuser, last_login, date_joined
     '''
     # for further information refer to https://docs.djangoproject.com/en/4.1/ref/contrib/auth/
 
     # custom fields
-    birthdate = models.DateField(default=timezone.now)
+    birthdate = models.DateField(auto_now=True)
     bio = models.CharField(default='', max_length=1000)
 
     def __str__(self):
