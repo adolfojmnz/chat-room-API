9c9e394b357e873a99de71be4b38eb96c418603f
Adolfo Jimenez <adolfoj@protonmail.com>
2023-03-17 10:29:16 -0400

    Add 'rest_framework' to INSTALLED_APPS

--- config/settings.py
+++ config/settings.py
@@ -22,6 +22,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # third party
+    'rest_framework',
+
     # my apps
     'accounts',
     'chatrooms',
