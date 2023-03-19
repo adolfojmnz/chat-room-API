7d71750888521723e1069f3e164b06c39cd6fe7a
Adolfo Jimenez <adolfoj@protonmail.com>
2023-03-19 12:14:02 -0400

    Install djoser

--- config/settings.py
+++ config/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
 
     # third party
     'rest_framework',
+    'djoser',
 
     # my apps
     'accounts',
--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('auth/', include('djoser.urls')),
     path('frontend/', include('frontend.urls')),
 ]
