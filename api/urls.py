847998ed510e29295183e348758203108dbded33
Adolfo Jimenez <adolfoj@protonmail.com>
2023-03-20 11:06:13 -0400

    Minor reformat: change the order of the endpoints URLs

--- api/urls.py
+++ api/urls.py
@@ -7,12 +7,15 @@ app_name = 'api'
 urlpatterns = [
     path('users', views.UserListView.as_view(), name='users'),
     path('users/<int:pk>', views.UserDetailView.as_view(), name='user-detail'),
+
+    path('messages', views.MessageListView.as_view(), name='messages'),
+    path('messages/<int:pk>', views.MessageDetailView.as_view(), name='message-detail'),
+
+    path('topics', views.TopicListView.as_view(), name='topics'),
+    path('topics/<int:pk>', views.TopicDetailView.as_view(), name='topic-detail'),
+
     path('chatrooms', views.ChatroomListView.as_view(), name='chatrooms'),
     path('chatrooms/<int:pk>', views.ChatroomDetailView.as_view(), name='chatroom-detail'),
     path('chatrooms/<int:pk>/messages', views.MessageListView.as_view(), name='chatroom-messages'),
     path('chatrooms/<int:pk>/participants', views.ChatroomParticipantListView.as_view(), name='chatroom-participants'),
-    path('messages', views.MessageListView.as_view(), name='messages'),
-    path('messages/<int:pk>', views.MessageDetailView.as_view(), name='message-detail'),
-    path('topics', views.TopicListView.as_view(), name='topics'),
-    path('topics/<int:pk>', views.TopicDetailView.as_view(), name='topic-detail'),
 ]
