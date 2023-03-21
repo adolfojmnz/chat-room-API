4b18ecf5b9a9d4db1466fac68d3a35eb1429a935
Adolfo Jimenez <adolfoj@protonmail.com>
2023-03-21 13:21:42 -0400

    Setup token-based authentication with drf-simpleJWT

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+
     path('auth/', include('djoser.urls')),
+
+    path('api/token/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     path('frontend/', include('frontend.urls')),
 ]
