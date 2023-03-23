709bc187bb75f8c5c184bbc99760e5114d73494a
Adolfo Jimenez <adolfoj@protonmail.com>
2023-03-22 21:34:08 -0400

    Setup postgresql database

--- config/settings.py
+++ config/settings.py
@@ -67,9 +67,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DATABASE_NAME'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+    },
 }
 
 AUTH_PASSWORD_VALIDATORS = [
