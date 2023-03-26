b8ce5032ae54cf8cb44513749975d7c3f019335e
Adolfo Jimenez <adolfoj@protonmail.com>
2023-03-26 14:38:27 -0400

    Fix broken tests

--- api/tests/test_chatroom_topics_endpoint.py
+++ api/tests/test_chatroom_topics_endpoint.py
@@ -1,91 +1,65 @@
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.urls import reverse
 
 from rest_framework import status
 
-import json
+from json import dumps
 
-from api import serializers
-from accounts.models import CustomUser as User
 from chatrooms.models import Chatroom, Topic
+from api.tests.mixins import (
+    UserMixin,
+    TopicMixin,
+    ChatroomMixin,
+    ChatroomTopicMixin,
+)
 
 
-class UserHelperMixin:
-    user_data = {
-        'username': 'test_username',
-        'first_name': 'test_first_name',
-        'last_name': 'test_last_name',
-        'email': 'test_email@localhost.com',
-        'bio': 'test_bio',
-        'birtdate': '1989-09-29',
-        'password': 'test_password',
-    }
-
-    def create_user(self):
-        return self.client.post(reverse('api:users'), self.user_data)
-
-
-class ChatroomHelperMixin:
-    chatroom_data = {
-        'name': 'test name',
-        'description': 'test description',
-        'creation_date': '2023-03-16',
-        'public': True,
-        'min_age_required': 13,
-    }
-
-    def create_chatroom(self):
-        return self.client.post(
-            reverse('api:chatrooms'),
-            self.chatroom_data,
-        )
-
-
-class TopicHelperMixin(UserHelperMixin, ChatroomHelperMixin):
-    topic_data = {
-        'name': 'test topic',
-        'description': 'test description',
-    }
+class SetUpMixin(UserMixin, TopicMixin, ChatroomMixin, ChatroomTopicMixin):
 
     def setUp(self):
-        self.client = Client()
-        self.url = reverse('api:chatroom-topics', kwargs={'pk': 1})
         self.create_user()
-        self.create_topic()
-        self.create_chatroom()
+        self.client = self.get_client_with_authorization_headers()
+        self.chatroom_response = self.create_chatroom()
+        self.topic_response = self.create_topic()
+        self.last_setup()
         return super().setUp()
 
-    def create_topic(self):
-        return Topic.objects.create(
-            name = self.topic_data['name'],
-            description = self.topic_data['description'],
-        )
 
-
-class TestChatroomTopicListEndpoint(TopicHelperMixin, TestCase):
+class TestChatroomTopicListEndpoint(SetUpMixin, TestCase):
 
     def setUp(self):
         return super().setUp()
+
+    def build_url(self):
+        self.url = reverse(
+            'api:chatroom-topics',
+            kwargs = {'pk': self.topic_response.data.get('id')},
+        )
+
+    def last_setup(self):
+        self.topic = Topic.objects.get(pk=self.topic_response.data.get('id'))
+        self.chatroom = Chatroom.objects.get(pk=self.chatroom_response.data.get('id'))
+        self.chatroom.topics.add(self.topic)
+        self.build_url()
 
     def test_get(self):
-        chatroom = Chatroom.objects.get(pk=1)
-        chatroom.topics.add(Topic.objects.get(pk=1))
         response = self.client.get(self.url)
-        serializer = serializers.TopicSerializer(chatroom.topics.all(), many=True)
+        serializer = self.get_list_chatroom_topic_serializer()
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, serializer.data)
-        self.assertNotEqual(response.data, [])
+        self.assertEqual(self.chatroom.topics.all().count(), 1)
 
     def test_post(self):
-        response = self.client.post(self.url, json.dumps({'id': 1}), content_type='application/json')
-        serializer = serializers.TopicSerializer(Chatroom.objects.get(pk=1).topics.all(), many=True)
+        self.chatroom.topics.remove(self.topic)
+        response = self.client.post(self.url, dumps({'id': self.topic.pk}), content_type='application/json')
+        serializer = self.get_list_chatroom_topic_serializer()
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, serializer.data)
-        self.assertNotEqual(response.data, [])
+        self.assertEqual(self.chatroom.topics.all().count(), 1)
 
     def test_delete(self):
-        response = self.client.delete(self.url, json.dumps({'id': 1}), content_type='application/json')
-        serializer = serializers.TopicSerializer(Chatroom.objects.get(pk=1).topics.all(), many=True)
+        response = self.client.delete(self.url, dumps({'id': self.topic.pk}), content_type='application/json')
+        serializer = self.get_list_chatroom_topic_serializer()
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, serializer.data)
-        self.assertEqual(response.data, [])
+        self.assertEqual(self.chatroom.topics.all().count(), 0)
