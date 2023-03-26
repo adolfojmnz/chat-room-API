cfa99618c4f9dd530845e3f4f5bd15a38dd10d67
Adolfo Jimenez <adolfoj@protonmail.com>
2023-03-26 15:04:07 -0400

    Temporarily disable pagination

--- config/settings.py
+++ config/settings.py
@@ -114,8 +114,8 @@ REST_FRAMEWORK = {
         'rest_framework.filters.OrderingFilter',
         'rest_framework.filters.SearchFilter',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE': 5
 }
 
 SIMPLE_JWT = {
